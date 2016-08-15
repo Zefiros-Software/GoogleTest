fa158053b4aad5a3b0efd9128c8ced5d94086abe
PaulVisscher <paul.pev@hotmail.com>
2016-08-15 14:26:50 +0200

    Appveyor added

---
+++ script/appveyor-buid.bat
@@ -0,0 +1,7 @@
+cd test/
+premake5 install-package --allow-install --allow-module  || exit /b 1
+premake5 vs2015 || exit /b 1
+msbuild serialisation/SerLib.sln /property:Configuration=Test /property:Platform=Win32 || exit /b 1
+
+
+.\bin\Test\GoogleTest.exe || exit /b 1
